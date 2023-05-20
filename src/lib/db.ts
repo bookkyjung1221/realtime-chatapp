a6fa8a11baff271bbead863f211897b1e948b65f
Pholphat Kittivittayakul <pholphat234@gmail.com>
2023-05-20 15:23:36 +0700

    final draft

---
+++ src/lib/db.ts
@@ -0,0 +1,6 @@
+import { Redis } from '@upstash/redis'
+
+export const db = new Redis({
+  url: <string>process.env.UPSTASH_REDIS_REST_URL,
+  token: <string>process.env.UPSTASH_REDIS_REST_TOKEN
+})
